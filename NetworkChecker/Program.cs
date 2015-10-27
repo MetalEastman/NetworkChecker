4ec19d33e6cdac871ed3792d03491e82154e3e4e
Fredrik <fredrikostman85@gmail.com>
2015-10-27 15:42:46 +0100

    Gets computer name and Ip

---
+++ NetworkChecker/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.DirectoryServices;
+
+namespace NetworkChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var root = new DirectoryEntry("WinNT:");
+
+            foreach (DirectoryEntry comps in root.Children)
+            {
+                foreach (DirectoryEntry c in comps.Children)
+                {
+                    if (c.Name != "Schema")
+                    {
+                        Console.WriteLine(c.Name);
+                        GetIpFromName(c.Name);
+                        Console.WriteLine("**************************");
+                    }
+                }
+
+            }
+
+            Console.ReadLine();
+        }
+
+        static void GetIpFromName(string comp)
+        {
+            IPHostEntry ipEntry = Dns.GetHostEntry(comp);
+            IPAddress[] ipAddr = ipEntry.AddressList;
+
+            Console.WriteLine(ipAddr[2]);    
+        }
+    }
+}
